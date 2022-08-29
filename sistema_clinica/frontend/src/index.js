e64b88b4e56312da895adf485872458523671eb1
Arthur Severo <62119941+Sevzera@users.noreply.github.com>
2022-08-29 11:22:50 -0300

    removido react-query & eslint

--- sistema_clinica/frontend/src/index.js
+++ sistema_clinica/frontend/src/index.js
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<QueryClientProvider client={queryClient}>
 		<App />
-	</QueryClientProvider>
 );
