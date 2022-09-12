53876753bf43648157c6ef8defe02cf1286c391f
Arthur Severo <62119941+Sevzera@users.noreply.github.com>
2022-09-12 20:25:53 -0300

    back

--- sistema_clinica/backend/src/routers/index.js
+++ sistema_clinica/backend/src/routers/index.js
@@ -1,6 +1,23 @@
-export { default as pessoa } from "./pessoa.js";
-export { default as paciente } from "./paciente.js";
-export { default as funcionario } from "./funcionario.js";
-export { default as medico } from "./medico.js";
-export { default as agenda } from "./agenda.js";
-export { default as endereco } from "./endereco.js";
+import express from "express";
+const router = express.Router();
+router.get("/", async (req, res) => {
+	const result = await controller.select(req.query);
+	res.json(result);
+});
+
+router.post("/", async (req, res) => {
+	const result = await controller.insert(req.body);
+	res.json(result);
+});
+
+router.put("/", async (req, res) => {
+	const result = await controller.update(req.body);
+	res.json(result);
+});
+
+router.delete("/", async (req, res) => {
+	const result = await controller.remove(req.body);
+	res.json(result);
+});
+
+export default router;
--- sistema_clinica/backend/src/server.js
+++ sistema_clinica/backend/src/server.js
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 dotenv.config();
 
-import * as routers from "./routers/index.js";
+import queryRouter from "./routers";
 
 export const server = async () => {
 	const server = express();
@@ -18,8 +18,12 @@ export const server = async () => {
 	});
 	server.use(express.json());
 
-	server.use("/api/agenda", routers.agenda);
-	server.use("/api/medico", routers.medico);
+	server.use("/api/agenda", queryRouter);
+	server.use("/api/endereco", queryRouter);
+	server.use("/api/funcionario", queryRouter);
+	server.use("/api/medico", queryRouter);
+	server.use("/api/paciente", queryRouter);
+	server.use("/api/pessoa", queryRouter);
 
 	const port = process.env.PORT || 1999;
 	server.listen(port, () => {
