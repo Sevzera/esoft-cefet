edd196260765dccb8e9ee4839374301dc02abee0
Arthur Severo <62119941+Sevzera@users.noreply.github.com>
2022-09-15 10:21:53 -0300

    conexao ao banco padrao

--- sistema_clinica/backend/src/database.js
+++ sistema_clinica/backend/src/database.js
@@ -2,8 +2,12 @@ import dotenv from "dotenv";
 import Sequelize from "sequelize";
 dotenv.config();
 
-const database = new Sequelize(process.env.DB_URL, {
-	dialect: process.env.DB_DIALECT
-});
+const database = new Sequelize(
+	process.env.DB_URL ||
+		"postgres://vympnzaq:o761azpZFIjcEoQeu7AQj2-PsGSM6IWB@jelani.db.elephantsql.com/vympnzaq",
+	{
+		dialect: process.env.DB_DIALECT || "postgres"
+	}
+);
 
 export default database;
